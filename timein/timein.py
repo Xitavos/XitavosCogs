2a376982d7de7b3adb5bf3d6c9c4afac04ff294b
Paul Jones <pj.pauljonesnz@gmail.com>
2017-01-13 01:51:04 +1300

    Added a search by city name. Moved both functions to subcommands

--- timein/timein.py
+++ timein/timein.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import datetime
 import os
 from .utils.dataIO import fileIO
+from __main__ import send_cmd_help
 import aiohttp
 try: # check if BeautifulSoup4 is installed
 	from bs4 import BeautifulSoup
@@ -17,18 +18,24 @@ class timein:
 		self.bot = bot
 		self.settings = fileIO("data/timein/settings.json", "load")
 
-	@commands.command()
-	async def timein(self, text):
-		"""Gets all current dates and times from all timezones in the specified country.
+	@commands.group(pass_context=True)
+	async def timein(self, ctx):
+		"""Gets all current dates and times from all timezones in the specified country."""
+		if ctx.invoked_subcommand is None:
+			await send_cmd_help(ctx)
+
+	@timein.command(name="country")
+	async def _timein_country(self, text):
+		"""Get time using country code
 		
-		Usage: timein [AA]
+		Usage: timein country [AA]
 		
-		where [AA] is a country code from this list https://timezonedb.com/country-codes
+		where [AA] is a 2 letter country code from this list https://timezonedb.com/country-codes or a custom shortcut code
 		
 		Preset shortcuts:
 		UK - United Kingdom (converts to GB)
 		USE - United States East (New York)
-		USW - United States West (Los Angeles)		
+		USW - United States West (Los Angeles)
 		"""
 		
 		apiKey = self.settings['api_key']
@@ -45,6 +52,9 @@ class timein:
 		elif text.lower() == 'usw':
 			url += '&country=US&zone=*Los_Angeles*'
 			flag += 'us: WEST '
+		elif text.lower() == 'test':
+			url += '&zone=*auckland*'
+			flag += 'nz: '
 		elif len(text) != 2 or ' ' in text == False:
 			await self.bot.say("Country code must be 2 letters and from this list https://timezonedb.com/country-codes")
 			return
@@ -58,18 +68,72 @@ class timein:
 			soupObject = BeautifulSoup(await response.text(), "html.parser")
 		message = ''
 		
-		zones = soupObject.find_all('zone')
-		for zone in zones:
-			newmessage = flag
-			newmessage += zone.find('countryname').get_text() + '\n'
-			newmessage += zone.find('zonename').get_text() + '\n'
-			unixtime = zone.find('timestamp').get_text()
-			time = datetime.datetime.fromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
-			newmessage += time + '\n'
-			message += newmessage + '\n'
+		status = soupObject.find('status').get_text()
+		if status != 'OK':
+			message += 'Request failed. Details:\n```'
+			message += status + '\n'
+			message += soupObject.find('message').get_text()
+			message += '```\nMake sure country code is from the list at https://timezonedb.com/country-codes'
+		else:
+			zones = soupObject.find_all('zone')
+			for zone in zones:
+				newmessage = ''
+				newmessage += flag
+				newmessage += zone.find('countryname').get_text() + '\n'
+				newmessage += zone.find('zonename').get_text() + '\n'
+				unixtime = zone.find('timestamp').get_text()
+				time = datetime.datetime.fromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
+				newmessage += time + '\n'
+				message += newmessage + '\n'
+		
+		await self.bot.say(message)
+
+
+	@timein.command(name="city")
+	async def _timein_city(self, *, text):
+		"""Get time by searching for a city.
+		
+		Usage: timein city [CITY NAME]
+		
+		Only capitals and other major cities are in this list
+		"""
+		
+		apiKey = self.settings['api_key']
+		if ".com" in apiKey:
+			await self.bot.say("You have to set your API key, see data/timein/settings.json for details")
+			return
+		
+		url = 'http://api.timezonedb.com/v2/list-time-zone?key=' + apiKey + '&format=xml'
+		
+		city = text.replace(' ', '_')
+		
+		url += '&zone=*' + city + '*'
+		
+		async with aiohttp.get(url) as response:
+			soupObject = BeautifulSoup(await response.text(), "html.parser")
+		message = ''
+		
+		status = soupObject.find('status').get_text()
+		if status != 'OK':
+			message += 'Request failed. Details:\n```'
+			message += status + '\n'
+			message += soupObject.find('message').get_text()
+			message += '```\nTry searching for a capital or other major city'
+		else:
+			zones = soupObject.find_all('zone')
+			for zone in zones:
+				newmessage = ''
+				newmessage += ':flag_' + zone.find('countrycode').get_text().lower() + ': '
+				newmessage += zone.find('countryname').get_text() + '\n'
+				newmessage += zone.find('zonename').get_text() + '\n'
+				unixtime = zone.find('timestamp').get_text()
+				time = datetime.datetime.fromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
+				newmessage += time + '\n'
+				message += newmessage + '\n'
 		
 		await self.bot.say(message)
 		
+
 def check_folders():
 	if not os.path.exists("data/timein"):
 		print("Creating data/timein folder...")
