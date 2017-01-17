8cf4ec997f12a3639a3239069a60aca2658e6d97
Paul Jones <pj.pauljonesnz@gmail.com>
2017-01-18 04:26:28 +1300

    More work on favourites

--- timein/timein.py
+++ timein/timein.py
@@ -2,7 +2,8 @@ import discord
 from discord.ext import commands
 import datetime
 import os
-from .utils.dataIO import fileIO
+from .utils.dataIO import dataIO
+from .utils import checks
 from __main__ import send_cmd_help
 import aiohttp
 import time
@@ -18,9 +19,9 @@ class timein:
 
 	def __init__(self, bot):
 		self.bot = bot
-		self.settings = fileIO("data/timein/settings.json", "load")
-		self.favourites = fileIO("data/timein/favourites.json", "load")
-		self.cache = fileIO("data/timein/cache.json", "load")
+		self.settings = dataIO.load_json("data/timein/settings.json")
+		self.favourites = dataIO.load_json("data/timein/favourites.json")
+		self.cache = dataIO.load_json("data/timein/cache.json")
 
 	@commands.group(pass_context=True)
 	async def timein(self, ctx):
@@ -136,27 +137,59 @@ class timein:
 				message += newmessage + '\n'
 		
 		await self.bot.say(message)
-
+	
+	@timein.command(name="add")
+	async def _timein_add(self, text):
+		"""Adds a favourite group of timezones that can be quickly accessed
+		"""
+		
+		await self.bot.say("test")
+		
+	@timein.command(name="del")
+	async def _timein_del(self, text):
+		"""Removes a favourite group of timezones from the list
+		"""
+		
+		await self.bot.say("test")
+		
+	@timein.command(name="list")
+	async def _timein_list(self):
+		"""Lists all favourite groups of timezones
+		"""
+		
+		message = 'Favourites\n```Name: Timezones\n'
+		
+		for fav in self.favourites:
+			message += fav + ': '
+			message += self.favourites[fav].replace(',', ', ').replace('_', ' ') + '\n'
+		
+		message += '```'
+		await self.bot.say(message)
+	
 	@timein.command(name="fav")
-	async def _timein_fav(self, text):
+	async def _timein_fav(self, *, text):
 		"""Display all the times you have set in your favourites
 		"""
 		
 		lastRefresh = int(self.cache['last_refresh'])
 		currentTime = int(time.time())
 		if lastRefresh - currentTime > 48*60*60: #2 days
-			await self.bot.say("Cache refresh in progress")
-			#self.refresh_cache(self)
+			await self.bot.say("") #this is here so it loads
+			#self.refresh_cache()
 			
 		utcTime = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
+		
+		if text not in self.cache:
+			await self.bot.say("Favourite does not exist")
+			return
 			
-		favourite = self.cache['favourite']
+		favourite = self.cache[text]
 		
 		favSplit = favourite.strip().split(',')
 		
 		message = ''
 		
-		for fav in favSplit:		
+		for fav in favSplit:
 			msgSplit = fav.strip().split('~')
 			
 			message += msgSplit[0]
@@ -168,11 +201,21 @@ class timein:
 			
 		await self.bot.say(message)
 		
-	
+		
+	@timein.command(name="refresh")
+	@checks.admin_or_permissions(administrator=True)
+	async def _timein_refresh(self):
+		"""Refresh the favourites cache
+		
+			Use if daylight saving has started/ended or if times are wrong. Takes a short while to complete, depending on how many favourites you have
+		"""
+		
+		await self.refresh_cache()
+
 	async def refresh_cache(self):
 		tasknum = 0
 		num_zones = 0
-		base_msg = "Gathering data. This may take a few seconds"
+		base_msg = "Cache refreshing. This may take a few seconds"
 		status = ' %d/%d timezones fetched' % (tasknum, num_zones)
 		msg = await self.bot.say(base_msg + status)
 		
@@ -180,52 +223,55 @@ class timein:
 		if ".com" in apiKey:
 			await self.bot.say("You have to set your API key, see data/timein/settings.json for details")
 			return
-		
-		favourites = self.favourites['favourite'] #need to get the name to allow multiple favourite lists
-		
+			
 		finalMessage = ''
-		
-		split = favourites.strip().split(',')
-		
-		num_zones = len(split)
-		status = status = ' %d/%d timezones fetched' % (tasknum, num_zones)
-		msg = await self._robust_edit(msg, base_msg + status)
-		
-		for fav in split:
-			tasknum += 1
-
-			url = 'http://api.timezonedb.com/v2/list-time-zone?key=' + apiKey + '&format=xml' + fav
-		
-			async with aiohttp.get(url) as response:
-				soupObject = BeautifulSoup(await response.text(), "html.parser")
-			message = ''
 			
-			status = soupObject.find('status').get_text()
-			if status != 'OK':
-				message += 'Request failed. Details:\n```'
-				message += status + '\n'
-				message += soupObject.find('message').get_text()
-			else:
-				zones = soupObject.find_all('zone')
-				for zone in zones:
-					newmessage = ''
-					newmessage += ':flag_' + zone.find('countrycode').get_text().lower() + ': '
-					newmessage += zone.find('countryname').get_text() + '\n'
-					newmessage += zone.find('zonename').get_text() + '\n'
-					unixtime = zone.find('timestamp').get_text()
-					prettyTime = datetime.datetime.fromtimestamp(int(unixtime)).strftime('%Y-%m-%d %H:%M:%S')
-					newmessage += prettyTime + '\n'
-					message += newmessage + '\n'
-			finalMessage += message
+		for group in self.favourites:
+			message = ',\n"' + group + '" : "'
+			items = self.favourites[group].strip().split(',')
 			
+			num_zones += len(items)
 			status = status = ' %d/%d timezones fetched' % (tasknum, num_zones)
 			msg = await self._robust_edit(msg, base_msg + status)
 			
-			if tasknum != num_zones:
+			for item in items:
+				tasknum += 1
+				newMessage = ''
+				url = 'http://api.timezonedb.com/v2/list-time-zone?key=' + apiKey + '&format=xml&zone=' + item
+				async with aiohttp.get(url) as response:
+					soupObject = BeautifulSoup(await response.text(), "html.parser")
+				status = soupObject.find('status').get_text()
+				if status != 'OK':
+					newMessage += 'Request failed. Details:\n```'
+					newMessage += status + '\n'
+					newMessage += soupObject.find('message').get_text()
+					newMessage += '```\nTimezone not found'
+				else:
+					zone = soupObject.find('zone') #zone names are unique so should only ever be one result
+					newMessage += ':flag_' + zone.find('countrycode').get_text().lower() + ': '
+					newMessage += zone.find('countryname').get_text() + '\\n'
+					newMessage += zone.find('zonename').get_text() + '\\n'
+					newMessage += '~' + zone.find('gmtoffset').get_text()
+					if tasknum != num_zones:
+						newMessage += ','
+				message += newMessage
+				status = status = ' %d/%d timezones fetched' % (tasknum, num_zones)
+				msg = await self._robust_edit(msg, base_msg + status)
 				time.sleep(2)
+			message += '"'
+			finalMessage += message
+			self.cache[group] = message #HELP
+		finalMessage += '\n'
 		await self._robust_edit(msg, finalMessage)
+		#TODO write to cache file
+		self.cache['last_refresh'] = str(int(time.time()))
+		f = "data/timein/cache.json"
+		dataIO.save_json(f, self.cache)
+		
+
 	
 	@timein.command(name="dev")
+	@checks.admin_or_permissions(administrator=True)
 	async def _timein_dev(self):
 		"""Prints an example of what the api returns
 		"""
@@ -255,23 +301,23 @@ def check_folders():
 		os.makedirs("data/timein")
 
 def check_files():
-	settings = {"api_key" : "Get your API key from: https://timezonedb.com",}
+	settings = {"api_key" : "Get your API key from: https://timezonedb.com"}
 	favourites = {}
-	cache = {"last_refresh" : "0", "fav" : "test",}
+	cache = {"last_refresh" : "0"}
 	
 	f = "data/timein/settings.json"
-	if not fileIO(f, "check"):
+	if not dataIO.is_valid_json(f):
 		print("Creating settings.json")
 		print("You must obtain an API key as noted in the newly created 'settings.json' file")
-		fileIO(f, "save", settings)
+		dataIO.save_json(f, settings)
 	f = "data/timein/favourites.json"
-	if not fileIO(f, "check"):
+	if not dataIO.is_valid_json(f):
 		print("Creating favourites.json")
-		fileIO(f, "save", favourites)
+		dataIO.save_json(f, favourites)
 	f = "data/timein/cache.json"
-	if not fileIO(f, "check"):
+	if not dataIO.is_valid_json(f):
 		print("Creating cache.json")
-		fileIO(f, "save", cache)
+		dataIO.save_json(f, cache)
 
 def setup(bot):
 	check_folders()
